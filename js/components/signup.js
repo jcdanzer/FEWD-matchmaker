08c615a6bcb43b0a4102ebb7bc7aa0c486415236
RickBL <rick@blendimages.com>
2015-10-13 18:14:17 -0700

    Working on user logic

--- js/components/signup.js
+++ js/components/signup.js
@@ -4,20 +4,26 @@ function postit(){
 
 console.log("passed to .js!")
 
+  var avatar = document.getElementById("avatar").object;
   var email = document.getElementById("email").value;
-  var user = document.getElementById("user").value;
-  var pass = document.getElementById("pass").value;
-  var first = document.getElementById("first").value;
-  var last = document.getElementById("last").value;
+  var user = document.getElementById("username").value;
+  var pass = document.getElementById("password").value;
+  var first = document.getElementById("firstName").value;
+  var last = document.getElementById("lastName").value;
 
   var options = {
+    avatar: avatar,
     email: email,
-    
+    username: username,
+    password: password,
+    lastName: lastname,
+    firstName: firstname
+
   };
 
   var http = new xhrHandler();
 
-  var req = http.request(function("POST", "URL", );
+  var req = http.request(function("POST", "http://vvvvvv.club/api", options);
 
   req(function success(){
 
