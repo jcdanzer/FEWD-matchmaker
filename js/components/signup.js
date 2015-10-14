777742f610efb5c512566c9e579b3101740a0591
RickBL <rick@blendimages.com>
2015-10-14 08:45:11 -0700

    labels to inputs, etc.
    cleaned up .css paths, added the reset.css file, figured out how to use
    on focus tag.  signup.js is throwing an error.  working on it.

--- js/components/signup.js
+++ js/components/signup.js
@@ -1,34 +1,34 @@
 // javascript page
 
-function postit(){
+function getFormValues(){
 
-console.log("passed to .js!")
-
-  var avatar = document.getElementById("avatar").object;
+  var avatar = document.getElementById("avatar").value;
   var email = document.getElementById("email").value;
   var user = document.getElementById("username").value;
   var pass = document.getElementById("password").value;
   var first = document.getElementById("firstName").value;
   var last = document.getElementById("lastName").value;
 
-  var options = {
-    avatar: avatar,
-    email: email,
-    username: username,
-    password: password,
-    lastName: lastname,
-    firstName: firstname
+console.log(email, user, pass, first, last)
 
-  };
+  // var options = {
+  //   avatar: avatar,
+  //   email: email,
+  //   username: username,
+  //   password: password,
+  //   lastName: lastname,
+  //   firstName: firstname
+  //
+  // };
+  //
+  // var http = new xhrHandler();
+  //
+  // //var req = http.request(function("POST", "http://vvvvvv.club/api", "options"));
+  //
+  // req(function success(){
+  //
+  // }, function error(){
 
-  var http = new xhrHandler();
-
-  var req = http.request(function("POST", "http://vvvvvv.club/api", options);
-
-  req(function success(){
-
-  }, function error(){
-
-  })
+//  })
 
 }
