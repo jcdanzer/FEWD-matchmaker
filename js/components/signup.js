7e47b591fed91c11f6a4f8b9d7947ccc0a1b5c63
RickBL <rick@blendimages.com>
2015-10-14 18:11:28 -0700

    Form works and passed to .js
    Need to put in a few validators (email for example), but I think just
    about ready to pass into vvvvvv.api

--- js/components/signup.js
+++ js/components/signup.js
@@ -1,34 +1,41 @@
 // javascript page
 
-function getFormValues(){
+function getFormValues(form){
 
-  var avatar = document.getElementById("avatar").value;
-  var email = document.getElementById("email").value;
-  var user = document.getElementById("username").value;
-  var pass = document.getElementById("password").value;
-  var first = document.getElementById("firstName").value;
-  var last = document.getElementById("lastName").value;
+var avatar = form.avatar.value
+var email = form.email.value
+var username = form.username.value
+var password = form.password.value
+var firstName = form.firstName.value
+var lastName = form.lastName.value
 
-console.log(email, user, pass, first, last)
-
-  // var options = {
-  //   avatar: avatar,
-  //   email: email,
-  //   username: username,
-  //   password: password,
-  //   lastName: lastname,
-  //   firstName: firstname
-  //
-  // };
-  //
-  // var http = new xhrHandler();
-  //
-  // //var req = http.request(function("POST", "http://vvvvvv.club/api", "options"));
-  //
-  // req(function success(){
-  //
-  // }, function error(){
-
-//  })
-
+console.log(avatar, email, username, password, firstName, lastName)
 }
+  // var avatar = document.getElementById("avatar").value;
+  // var email = document.getElementById("email").value;
+  // var user = document.getElementById("username").value;
+  // var pass = document.getElementById("password").value;
+  // var first = document.getElementById("firstName").value;
+  // var last = document.getElementById("lastName").value;
+
+
+
+ //  var options = {
+ //    avatar: avatar,
+ //    email: email,
+ //    username: username,
+ //    password: password,
+ //    lastName: lastname,
+ //    firstName: firstname
+ //
+ //  };
+ //
+ //  var http = new xhrHandler();
+ //
+ //  //var req = http.request(function("POST", "http://vvvvvv.club/api", "options"));
+ //
+ //  req(function success(){
+ //
+ //  }, function error(){
+ //
+ // })
