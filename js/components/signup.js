951e8aab5dd0a4eea18e6c452cfd3504b19fa9be
RickBL <rick@blendimages.com>
2015-10-16 10:56:27 -0700

    testing update procedure

--- js/components/signup.js
+++ js/components/signup.js
@@ -25,41 +25,30 @@ function previewFile() {
   }
 
     if (file) {
-    //  var avatarimage = reader.readAsDataURL(file);
       reader.readAsDataURL(file);
     } else {
       preview.src = "";
     }
 
 
-// var newuser = {
-//   avatar: {
-//     image:reader.result
-//   },
-//   email: email,
-//   username: username,
-//   password: password,
-//   lastName: lastName,
-//   firstName: firstName
-// }
-// console.log(newuser);
+    var newuser = {
+      avatar: {
+        image:reader.result
+      },
+      email: email,
+      username: username,
+      password: password,
+      lastName: lastName,
+      firstName: firstName
+    }
+    console.log(newuser);
+
+
+  var http = new xhrHandler();
+
+  //var req = http.request(function("POST", "http://vvvvvv.club/api", newuser));
+
 };
-
-
-
-  // var avatar = document.getElementById("avatar").value;
-  // var email = document.getElementById("email").value;
-  // var user = document.getElementById("username").value;
-  // var pass = document.getElementById("password").value;
-  // var first = document.getElementById("firstName").value;
-  // var last = document.getElementById("lastName").value;
-
-
-// SEND TO API
-
- //  var http = new xhrHandler();
- //
- //  var req = http.request(function("POST", "http://vvvvvv.club/api", newuser));
  //
  //  req(function success(){
  //
