c7f62df14bfdd040073bd2bb41be386ad288312f
lareeee <lareeross@Laree-Rosss-MacBook-Pro.local>
2015-10-21 17:50:16 -0700

    Team Page Updates

--- js/components/team.js
+++ js/components/team.js
@@ -1,22 +1,72 @@
-var Menu = function(data){
-  //this.list = [];
-//this.addForm = title;
+var Menu = function(){
+  this.list = [];
   //this.open = true;
 
   this.elem = document.createElement('div');
   this.addForm = document.createElement('div');
-  this.menu = document.createElement('menu');
+  this.ul = document.createElement('ul');
 
-  this.elem.appendChild(this.menu);
   this.elem.appendChild(this.addForm);
+  this.elem.appendChild(this.ul);
 
   this.elem.classList.add('menu');
   this.addForm.classList.add('add-form');
 
-  this.addForm.innerHTML = '<input type="text"><div class="plus"></div>';
+  this.addForm.innerHTML = '<input type="text" placeholder="team name"><div class="add"></div>';
+
+
+// select the element with a class 'add' and make it open the form and focus the input field
+this.addForm.querySelectorAll('.add')[0].addEventListener('click', function(){
+
+  var item = {
+    content: this.addForm.querySelectorAll('input')[0].value
+  };
+  // add the item, reset the input, and close the form
+  this.addItem(item);
+  this.addForm.querySelectorAll('input')[0].value = '';
+
+}.bind(this));
+
+// listen for when the user types in the input
+this.addForm.querySelectorAll('input')[0].addEventListener('keydown',function(ev){
+  // if the user presses return
+  if(ev.keyCode === 13) {
+    // create a valid model for the data and set its content
+    var item = {
+      content: this.addForm.querySelectorAll('input')[0].value
+    };
+    // add the item, reset the input, and close the form
+    this.addItem(item);
+    this.addForm.querySelectorAll('input')[0].value = '';
+  }
+}.bind(this));
 
   document.getElementById('container').appendChild(this.elem);
-}
 
-//{
-  //}.bind(this));
+};
+
+// function that adds an item
+Menu.prototype.addItem = function(item) {
+  // push the item to the list
+  this.list.push(item);
+  // create a li
+  var listItem = document.createElement('li');
+  // set the content of the li
+  listItem.innerHTML = '<span>'+item.content+'</span>';
+  // select the checkbox and listen for clicks
+
+  // add a class for styling
+  listItem.classList.add('menu-item');
+  // append the li
+  this.ul.appendChild(listItem);
+
+};
+
+
+Menu.prototype.createList = function(list) {
+    // we can loop through the array easily with forEach
+    list.forEach(function(item, index){
+        this.addItem(item);
+    }.bind(this));
+
+};
--- js/components/tiles.js
+++ js/components/tiles.js
@@ -2,13 +2,20 @@ var Tiles = function(users){
 
   this.elem = document.createElement('div');
   this.ul = document.createElement('ul');
-  this.header = document.createElement('header');
 
-  this.elem.appendChild(this.header);
   this.elem.appendChild(this.ul);
 
   this.elem.classList.add('tiles');
 
+//  users.forEach(function(users){
+//    var li = document.createElement('li');
+//    this.ul.appendChild(li);
+
+//    li.innerHTML = '<img src="'+card.avatar.image+'"><div class="email"><h2>'+object.email+'</h2></div>';
+
+
+//  }.bind(this));
+
   document.getElementById('container').appendChild(this.elem);
 
-}
+};
