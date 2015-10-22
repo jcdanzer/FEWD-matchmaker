e826a9b2c0b1c78aa47d52de5d228cde427c3903
lareeee <lareeross@Laree-Rosss-MacBook-Pro.local>
2015-10-21 19:37:43 -0700

    tile updates

--- js/components/tiles.js
+++ js/components/tiles.js
@@ -7,14 +7,14 @@ var Tiles = function(users){
 
   this.elem.classList.add('tiles');
 
-//  users.forEach(function(users){
-//    var li = document.createElement('li');
-//    this.ul.appendChild(li);
-
-//    li.innerHTML = '<img src="'+card.avatar.image+'"><div class="email"><h2>'+object.email+'</h2></div>';
-
-
-//  }.bind(this));
+  // users.forEach(function(res){
+  //  var li = document.createElement('li');
+  //  this.ul.appendChild(li);
+  //
+  //  li.innerHTML = '<img src="'+"http://vvvvvv.club"+res.avatar'"><div class="email"><h2>'&#64;'+'+res.username+'</h2></div>';
+  // 
+  //
+  // }.bind(this));
 
   document.getElementById('container').appendChild(this.elem);
 
