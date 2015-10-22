0b7fadd55124c21051747b9cd7422fc7b41ab403
RickBL <rick@blendimages.com>
2015-10-21 19:42:15 -0700

    more stuff

--- js/components/getusers.js
+++ js/components/getusers.js
@@ -1,13 +1,23 @@
 //GET USERS FROM API
 
-
 var xhr= new xhrHandler();
 
-function getUsers(userlist){
+xhr.request('POST',"http://vvvvvv.club/api/login", {"username": "admin", "password": "password"}).then(function(data){
+  console.log('hey i"m logged in!');
+  getUsers();  
+});
+
+
+
+
+function getUsers(){
 
     xhr.request('GET', "http://vvvvvv.club/api/user","").then(function(res){
       console.log(res);
-      
+
+
+new Tiles(res)
+
 
 // random tests of function to see if I can manipulate "res" object
 
@@ -15,20 +25,19 @@ function getUsers(userlist){
       console.log(response);
       console.log(res[5].username);
       console.log("in between")
-      console.log(res[32].username);
+
 
       for (var i=0, len = response; i<len; i++){
-        console.log(res[i].username, res[i].lastName, res[i].email)}
+        console.log(res[i].username, res[i].firstName, res[i].lastName, res[i].email, "http://vvvvvv.club/",res[i].avatar)}
 
     });
 };
 
-function login(){
-xhr.request('POST',"http://vvvvvv.club/api/login", {"username": "admin", "password": "password"})
-}
 
-login();
-getUsers();
+
+
+
+
 
 
 //getElementById('welcome')
--- js/components/tiles.js
+++ js/components/tiles.js
@@ -1,5 +1,9 @@
 var Tiles = function(users){
 
+
+console.log("THIS IS THE TILE FUNCTION")
+console.log(users)
+
   this.elem = document.createElement('div');
   this.ul = document.createElement('ul');
 
@@ -7,14 +11,14 @@ var Tiles = function(users){
 
   this.elem.classList.add('tiles');
 
-//  users.forEach(function(users){
-//    var li = document.createElement('li');
-//    this.ul.appendChild(li);
+ users.forEach(function(users){
+   var li = document.createElement('li');
+   this.ul.appendChild(li);
 
-//    li.innerHTML = '<img src="'+card.avatar.image+'"><div class="email"><h2>'+object.email+'</h2></div>';
+   li.innerHTML = '<img src="'+card.avatar.image+'"><div class="email"><h2>'+object.email+'</h2></div>';
 
 
-//  }.bind(this));
+ }.bind(this));
 
   document.getElementById('container').appendChild(this.elem);
 
